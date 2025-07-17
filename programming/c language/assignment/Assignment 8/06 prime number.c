54371cc6d0e9edc78caa77c670b8b25c4e39559e
Gopal Joshi <cdt.gopaljoshi0411@gmail.com>
2025-07-17 18:46:37 +0530

    Add files via upload

---
+++ programming/c language/assignment/Assignment 8/06 prime number.c
@@ -0,0 +1,20 @@
+//Accept array and print only prime numbers of array
+#include<stdio.h>
+void main()
+{
+	int arr[10];
+	printf("enter a number in value =");
+	for(int i=0;i<10;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0;i<10;i++)
+	{
+		printf("%d",arr[i]);
+	}
+	printf("print only prime number = \n");
+	for(int i=0;i<10;i++)
+	{
+		
+	}
+}
